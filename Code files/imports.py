e4676741f1497e494ff4d973678e62acadc44aa6
tonyhasson <63504193+tonyhasson@users.noreply.github.com>
2021-12-20 20:32:31 +0200

    Create imports.py

---
+++ Code files/imports.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+#import seaborn as sns
+from collections import Counter
+#import sklearn
+import bs4
+from bs4 import BeautifulSoup
+import requests
+# from sklearn import linear_model, metrics, preprocessing
+# from sklearn.preprocessing import StandardScaler, MinMaxScaler
+# from sklearn.linear_model import LogisticRegression, LinearRegression
+# from sklearn.metrics import r2_score, f1_score
+# import scipy as sc
