06b8ae86698761857688aaa56d99afe5dd11c580
BiggieFudge <eytanrhl@gmail.com>
2021-12-21 19:19:56 +0200

    update to Quality of life and add CSV

--- Code files/QualityOfLife.py
+++ Code files/QualityOfLife.py
@@ -1,45 +1,45 @@
-import bs4
+from bs4 import BeautifulSoup
 import pandas
 import pandas as pd
 import requests
-import imports
+#import imports
 # Made By Eytan
 
 def load_soup_object(year):
     url = 'https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=' + str(year)
     content = requests.get(url)
-    r =bs4.BeautifulSoup(content.text,'html.parser')
+    r =BeautifulSoup(content.text,'html.parser')
     return r
 
-def create_col_list(txt):
-
-    col_names = txt.split("\n")
-    new_col = []
-    for i in col_names:
-        if i != '':
-            new_col.append(i)
-    col_names=[]
-    for j in new_col:
-        if len(j)!=1:
-            col_names.append(j)
-    return col_names
 
 
 def create_dataframe():
-    for i in range(2021,2022):
+    for i in range(2012,2022):
         column=[]
-        html_file=load_soup_object(i)
+        if i != 2013 & i != 2014:
+            html_file=load_soup_object(i)
+        else:
+            html = open(r'C:\Users\eytan\PycharmProjects\ETZ-GDP\Quality of Life Index by Country '+ str(i)+'.html' ,encoding='utf-8')
+            html_file=BeautifulSoup(html.read(), 'html.parser')
         items=[]
         row=[]
         table =html_file.find('table',id='t2')
         for th in table.findAll('th'):
             column.append(th.text)
-
-        for tr in html_file.find('tbody'):
-            text=create_col_list(tr.text)
-            row.append(text)
-        print(row)
-        #df = pd.DataFrame(items, columns=column)
-        #print(df)
+        flag=0
+        column.pop(0)
+        for tr in html_file.findAll('tbody'):
+            for td in tr.findAll('td'):
+                if td.text !='':
+                    row.append(td.text)
+                    flag=1
+                elif flag==1:
+                    items.append(row)
+                    row =[]
+        df = pd.DataFrame(items, columns=column)
+        df.drop(['Cost of Living Index','Property Price to Income Ratio'],axis=1,inplace=True)
+        df.to_csv(r'C:\Users\eytan\PycharmProjects\ETZ-GDP\CSV files\QualityOfLife\'' + str(i) + '.csv')
+        if i == 2012:
+            print(df)
 
 create_dataframe()
