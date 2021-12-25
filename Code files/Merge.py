56967c203c279f6acc95282d942464e3c5462ad9
BiggieFudge <73502334+BiggieFudge@users.noreply.github.com>
2021-12-25 22:18:38 +0200

    update to merge file

--- Code files/Merge.py
+++ Code files/Merge.py
@@ -1,7 +1,7 @@
 from imports import *
 
-CSV_FILES=['Scraping CSV\df1','Scraping CSV\df2','Scraping CSV\df3','Scraping CSV\df4','Education RankingREFORMAT','Final consumption expenditureREFORMAT','GDP GrowthREFORMAT','GDP TotalREFORMAT','Government ExpenditureREFORMAT','Government Expense(of total GDP)REFORMAT','High Tech Exports(% of total)REFORMAT','High Tech Exports(total)REFORMAT','Life expectancy at birthREFORMAT','Population Growth paceREFORMAT','Population TotalREFORMAT','Military Expenditure totalREFORMAT','Military Expenditure(% of GDP)REFORMAT']
-List_Of_Countries=['Afghanistan','Albania','Algeria','Andorra','Angola','Antigua and Barbuda','Aruba','Argentina','Armenia','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','Brunei','Bulgaria','Burkina Faso','Burundi','Ivory Coast',r'Côte dIvoire','Cabo Verde','Cambodia','Cameroon','Canada','Central African Republic','Chad','Chile','China','Colombia','Comoros','Congo (Congo-Brazzaville)','Costa Rica','Croatia','Cuba','Cyprus','Czechia (Czech Republic)','Democratic Republic of the Congo','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Eswatini','Ethiopia','Fiji','Finland','France','Gabon','Gambia','Georgia','Germany','Ghana','Greece','Grenada','Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Holy See','Honduras','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Israel','Italy','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Mauritania','Mauritius','Mexico','Micronesia',',Moldova','Monaco','Mongolia','Montenegro','Morocco','Mozambique','Myanmar','Namibia','Nauru','Nepal','Netherlands','New Zealand','Nicaragua,','Niger','Nigeria','North Korea','North Macedonia','Norway','Oman','Pakistan','Palau','Palestine State','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal','Qatar','Romania','Russia','Rwanda','Saint Kitts and Nevis','Saint Lucia','Saint Vincent and the Grenadines','Samoa,','San Marino','Sao Tome and Principe','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','Solomon Islands','Somalia','South Africa','South Korea','South Sudan','Spain','Sri Lanka','Sudan','Suriname','Sweden','Switzerland','Syria','Tajik,istan','Tanza,nia','Thailand','Timor-Leste','Togo','Tonga','Trinidad and Tobago','Tunisia','Turkey','Turkmenistan','Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States of America','Uruguay','Uzbekistan','Vanuatu','Venezuela','Vietnam','Yemen','Zambia','Zimbabwe']
+CSV_FILES=['Education RankingREFORMAT','Final consumption expenditureREFORMAT','GDP GrowthREFORMAT','GDP TotalREFORMAT','Government ExpenditureREFORMAT','Government Expense(of total GDP)REFORMAT','High Tech Exports(% of total)REFORMAT','High Tech Exports(total)REFORMAT','Life expectancy at birthREFORMAT','Population Growth paceREFORMAT','Population TotalREFORMAT','Military Expenditure totalREFORMAT','Military Expenditure(% of GDP)REFORMAT']
+List_Of_Countries=['Afghanistan','Albania','Algeria','Andorra','Angola','Antigua and Barbuda','Aruba','Argentina','Armenia','Australia','Austria','Azerbaijan','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','Bulgaria','Burkina Faso','Burundi','Ivory Coast',r"Cote d'Ivoire",'Cabo Verde','Cambodia','Cameroon','Canada','Central African Republic','Chad','Chile','China','Colombia','Comoros','Congo (Congo-Brazzaville)','Costa Rica','Croatia','Cuba','Cyprus','Czechia (Czech Republic)','Democratic Republic of the Congo','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Eswatini','Ethiopia','Fiji','Finland','France','Gabon','Georgia','Germany','Ghana','Greece','Grenada','Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Holy See','Honduras','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Israel','Italy','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Mauritania','Mauritius','Mexico','Micronesia',',Moldova','Monaco','Mongolia','Montenegro','Morocco','Mozambique','Myanmar','Namibia','Nauru','Nepal','Netherlands','New Zealand','Nicaragua,','Niger','Nigeria','North Korea','North Macedonia','Norway','Oman','Pakistan','Palau','Palestine State','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal','Qatar','Romania','Russia','Rwanda','Samoa,','San Marino','Sao Tome and Principe','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','Solomon Islands','Somalia','South Africa','South Korea','South Sudan','Spain','Sri Lanka','Sudan','Suriname','Sweden','Switzerland','Syria','Tajik,istan','Tanza,nia','Thailand','Timor-Leste','Togo','Tonga','Trinidad and Tobago','Tunisia','Turkey','Turkmenistan','Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States','Uruguay','Uzbekistan','Vanuatu','Venezuela','Vietnam','Yemen','Zambia','Zimbabwe']
 
 
 def reformatCSV(CSV_location,CSV_name,Start_year,End_year):
@@ -24,10 +24,11 @@ def reformatCSV(CSV_location,CSV_name,Start_year,End_year):
     reformated.to_csv(r"..\CSV files\\" + CSV_name + 'REFORMAT.csv')
 
 
-def merge_and_clean(arr_df):
+def merge_and_clean(arr_df,Name):
     """Merge all dataframes into one and clean it a bit.
 
     Args:
+        Name (string): The name of the dataframe.
         arr_df (array): array of dataframes to manipulate.add()
 
     Returns:
@@ -42,17 +43,22 @@ def merge_and_clean(arr_df):
     df=arr_df[len(arr_df)-1]
     df.sort_values(['Country','Year'], axis=0, ascending=True, inplace=True)
     df.drop(['Unnamed: 0_x', 'Unnamed: 0_y'], axis=1, inplace=True)
+
+    ## change "Cote d'Ivoire" to "Ivory coast"
+    df.loc[df['Country']=="Cote d'Ivoire",'Country']='Ivory Coast'
+
     # remove all rows where year<1960
     df=df[df['Year']>=1960]
+
     # remove all unknown and irrelevant countries
     df=df[df['Country'].isin(List_Of_Countries)]
 
-    print(df.isnull().sum().sum())
-    print(df.shape[0])
+
+    df.to_csv(r"..\CSV files\\"+ Name +".csv")
     return df
 
 
-def arr_df_builder():
+def arr_df_builder(CSV_FILES):
     """Function to build Array of dataframes from CSV files.
 
     Returns:
@@ -68,6 +74,8 @@ def arr_df_builder():
     return arr_df
 
 
+
+
 # reformatCSV(r"..\CSV files\OLD\\","Education Ranking",1990,2019)
 # reformatCSV(r"..\CSV files\OLD\\","GDP Growth",1960,2020)
 # reformatCSV(r"..\CSV files\OLD\\","GDP Total",1960,2020)
@@ -76,9 +84,9 @@ def arr_df_builder():
 # reformatCSV(r"..\CSV files\OLD\\","High Tech Exports(total)",2007,2020)
 # reformatCSV(r"..\CSV files\OLD\\","Final consumption expenditure",1960,2020)
 # reformatCSV(r"..\CSV files\OLD\\","Population Growth pace",1960,2020)
-# reformatCSV(r"..\CSV files\OLD\\","Population Total",1960,2020)
+# reformatCSV(r"..\CSV files\OLD\\","Population Total",1960,2002)
 
-arr_df=arr_df_builder()
-merge_and_clean(arr_df).to_csv(r"..\CSV files\df_Total.csv")
-
-
+arr_df=arr_df_builder(CSV_FILES)
+scrap_arr_df=arr_df_builder(['Scraping CSV\df1','Scraping CSV\df2','Scraping CSV\df3','Scraping CSV\df4'])
+raw_data=merge_and_clean(arr_df,"df_Full_DataBase")
+scrap_raw_data=merge_and_clean(scrap_arr_df,"df_scrape")
