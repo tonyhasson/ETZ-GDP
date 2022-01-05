8fc2d037adcc4e79131e0465cee2882fcef30985
ziviser <zivi4444@gmail.com>
2022-01-05 19:42:58 +0200

    Visuals: removed todo list

--- Code files/main.py
+++ Code files/main.py
@@ -3,21 +3,6 @@ from Merge import Run as Run_Merge
 from ML_Algo import Run as Run_ML
 
 
-##############################################################################
-##################                TO DO LIST                ##################
-##############################################################################
-#   1. Clean the data                                                        #
-#   2. Analyze the data                                                      #
-#   3. Correlation between different things                                  #
-#   4. Make plots                                                            #
-#   5.                                                                       #
-#   6.                                                                       #
-#   7.                                                                       #
-#   8.                                                                       #
-#   9.                                                                       #
-#   10.                                                                      #
-##############################################################################
-
 if __name__ == "__main__":
     DF_FULL, DF_SCRAPE = Run_Merge()
     DF_FULL, DF_SCRAPE = Run_ML()
