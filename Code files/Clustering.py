e4782ea615b578d3e2bbd613272c70ca65cc0986
ziviser <zivi4444@gmail.com>
2022-01-16 14:26:54 +0200

    UPDATE: fixed typos and Applied Black Parser

--- Code files/Clustering.py
+++ Code files/Clustering.py
@@ -3,10 +3,11 @@ from imports import *
 
 FULL_DB_PATH = r"../CSV files/df_Full_DataBase.csv"
 SCRAP_DB_PATH = r"../CSV files/df_scrape.csv"
-REMOVE_COLUMN = ["Continent" ,"Country" ,"Year"]
+REMOVE_COLUMN = ["Continent", "Country", "Year"]
+
 
 def get_best_num_of_clusters_for_k_means(
-        dataset, num_cluster_options, init_val="k-means++", n_init_val=10, rand_state=None
+    dataset, num_cluster_options, init_val="k-means++", n_init_val=10, rand_state=None
 ):
     """Check and return the best number of clusters for k-means clustering
 
@@ -30,7 +31,9 @@ def get_best_num_of_clusters_for_k_means(
     return best_score, num_clusters
 
 
-def perform_k_means(dataset, num_clusters, init_val="k-means++", n_init_val=10, rand_state=None):
+def perform_k_means(
+    dataset, num_clusters, init_val="k-means++", n_init_val=10, rand_state=None
+):
     """Perform k-means clustering
 
     Args:
@@ -97,7 +100,6 @@ def get_best_params_for_dbscan(dataset, eps_options, min_samples_options):
                 pass
                 # print("fail ->  eps:%f min_samp:%f"%(eps,min_samples))
 
-
     return best_score, best_eps, best_min_samples
 
 
@@ -152,7 +154,7 @@ def Cluster_Graphs(name):
             ],
             "k-means++",
             15,
-            5
+            5,
         )
 
         # Perform k-means clustering
@@ -162,7 +164,12 @@ def Cluster_Graphs(name):
 
         fig, axes = plt.subplots(1, 2, figsize=(20, 5))
         fig.suptitle("k_means")
-        axes[0].set_title("World Clusters -  num_clusters:" + str(num_clusters )+ "- score:" + str(score))
+        axes[0].set_title(
+            "World Clusters -  num_clusters:"
+            + str(num_clusters)
+            + "- score:"
+            + str(score)
+        )
         axes[0].scatter(
             data1[column[0]], data1[column[1]], c=data1["Cluster"], s=50, cmap="plasma"
         )
@@ -182,9 +189,7 @@ def Cluster_Graphs(name):
 
         plt.show()
 
-
-
-        ##dbscan
+        # dbscan
 
         # Prepate the data to cluster
         data = data[data["Year"] == 2020]
@@ -193,20 +198,33 @@ def Cluster_Graphs(name):
         try:
             # Get the best number of clusters for DBSCAN
             best_score, best_eps, best_min_samples = get_best_params_for_dbscan(
-                data1.loc[:, data1.columns != "Continent"] ,[d for d in np.arange(0.1, 10, 0.1)],  [2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10])
+                data1.loc[:, data1.columns != "Continent"],
+                [d for d in np.arange(0.1, 10, 0.1)],
+                [2, 3, 4, 5, 6, 7, 8, 9, 10],
+            )
 
             # Perform DBSCAN
 
-            model, DBSCANData = perform_density_based_clustering( data1.loc[:, data1.columns != "Continent"] ,best_eps
-                                                                 ,best_min_samples)
+            model, DBSCANData = perform_density_based_clustering(
+                data1.loc[:, data1.columns != "Continent"], best_eps, best_min_samples
+            )
             # Add the cluster column to the dataframe
             data1["Cluster"] = DBSCANData
 
             fig, axes = plt.subplots(1, 2, figsize=(20, 5))
             fig.suptitle("DBSCAN")
-            axes[0].set_title("World Clusters -  epsilon" + str(best_eps )+ "- score:" + str(best_score))
+            axes[0].set_title(
+                "World Clusters -  epsilon"
+                + str(best_eps)
+                + "- score:"
+                + str(best_score)
+            )
             axes[0].scatter(
-                data1[column[0]], data1[column[1]], c=data1["Cluster"], s=50, cmap="plasma"
+                data1[column[0]],
+                data1[column[1]],
+                c=data1["Cluster"],
+                s=50,
+                cmap="plasma",
             )
             axes[0].set_xlabel(column[0])
             axes[0].set_ylabel(column[1])
@@ -222,13 +240,9 @@ def Cluster_Graphs(name):
                 )
             axes[1].legend()
 
-
             plt.show()
         except:
             pass
-
-
-
 
 
 def PCA_Total_graph(data):
@@ -294,26 +308,32 @@ def PCA_Cluster_Graph(data):
         5,
     )
     best_score, best_eps, best_min_samples = get_best_params_for_dbscan(
-        data[["principal component 1", "principal component 2"]] ,[d for d in np.arange(0.05, 1, 0.01)],  [2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10])
+        data[["principal component 1", "principal component 2"]],
+        [d for d in np.arange(0.05, 1, 0.01)],
+        [2, 3, 4, 5, 6, 7, 8, 9, 10],
+    )
     model, KmeansData = perform_k_means(
         data[["principal component 1", "principal component 2"]], num_clusters
     )
-    model, DBSCANData = perform_density_based_clustering(data[["principal component 1", "principal component 2"]]
-                                                         ,best_eps ,best_min_samples)
+    model, DBSCANData = perform_density_based_clustering(
+        data[["principal component 1", "principal component 2"]],
+        best_eps,
+        best_min_samples,
+    )
 
     data["kmean-cluster"] = KmeansData
     data["dbscan-cluster"] = DBSCANData
 
     # Kmean scatter plot
-    ComparePlot(data ,num_clusters ,score ,"kmean-cluster")
+    ComparePlot(data, num_clusters, score, "kmean-cluster")
 
     # DBScan scatter plot
-    ComparePlot(data, best_eps ,best_score ,"dbscan-cluster")
+    ComparePlot(data, best_eps, best_score, "dbscan-cluster")
 
     return data
 
 
-def ComparePlot(data ,num_clusters, score, label):
+def ComparePlot(data, num_clusters, score, label):
 
     fig, axes = plt.subplots(1, 2, figsize=(20, 5))
     fig.suptitle(label)
@@ -338,20 +358,20 @@ def ComparePlot(data ,num_clusters, score, label):
     plt.show()
 
 
-##send DataFrame here and max amount of neighbors to find best epsilon for DBscan
-def best_epsilon(data ,max_neighbors):
-    dat a =FULL_data.copy()
-    dat a =data.drop(columns=REMOVE_COLUMN)
+# send DataFrame here and max amount of neighbors to find best epsilon for DBscan
+def best_epsilon(FULL_data, max_neighbors):
+    data = FULL_data.copy()
+    data = data.drop(columns=REMOVE_COLUMN)
 
-    for n in range(2 ,max_neighbor s +1):
+    for n in range(2, max_neighbors + 1):
         neigh = NearestNeighbors(n_neighbors=n)
         nbrs = neigh.fit(data)
         distances, indices = nbrs.kneighbors(data)
 
         distances = np.sort(distances, axis=0)
-        distances = distances[: ,1]
+        distances = distances[:, 1]
         plt.plot(distances)
-        plt.title("n_neighbors :%d " %n)
+        plt.title("n_neighbors :%d " % n)
         plt.show()
 
 
