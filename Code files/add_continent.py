0fe5e05de989ef50f55dc6a4f52b85479dcc6f45
ziviser <zivi4444@gmail.com>
2022-01-29 22:38:14 +0200

    Update add_continent.py
    Fixed Comments

--- Code files/add_continent.py
+++ Code files/add_continent.py
@@ -26,16 +26,13 @@ def return_cont(country_name):
     return None
 
 
-# Driver Code: df_Continent
-
+## Driver Code: df_Continent
 # df_test = pd.read_csv(r"..\CSV files\df_test.csv")
 # country_list = [c for c in df_test["Country"].unique()]
 # continent_list = map(return_cont, country_list)
 # continent_list = list(continent_list)
-
 # new_df = pd.DataFrame(
 #     data={"Country": country_list, "Continent": continent_list},
 #     columns=["Country", "Continent"],
 # )
-
 # new_df.to_csv(r"..\CSV files\df_Continent.csv")
